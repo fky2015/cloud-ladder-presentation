0cd6c0f2bb450b1beeb6795c1bd91b414bdf24d0
FKYnJYQ <loveress01@gmail.com>
2020-05-29 12:59:17 +0800

    feat: 支持音频

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,43 @@ export default {
     let deck = new Reveal({
       plugins: [Markdown, Highlight]
     });
-    deck.initialize();
+    deck.initialize({
+      dependencies: [
+        // ...
+        {
+          src: "plugin/audio-slideshow/RecordRTC.js",
+          condition: function() {
+            return !!document.body.classList;
+          }
+        },
+        {
+          src: "plugin/audio-slideshow/slideshow-recorder.js",
+          condition: function() {
+            return !!document.body.classList;
+          }
+        },
+        {
+          src: "plugin/audio-slideshow/audio-slideshow.js",
+          condition: function() {
+            return !!document.body.classList;
+          }
+        }
+        // ...
+      ],
+      keyboard: {
+        82: function() {
+          console.debug("82")
+          Recorder.toggleRecording();
+        }, // press 'r' to start/stop recording
+        90: function() {
+          Recorder.downloadZip();
+        }, // press 'z' to download zip containing audio files
+        84: function() {
+          Recorder.fetchTTS();
+        } // press 't' to fetch TTS audio files
+      }
+    });
+    window.Reveal = deck;
   }
 };
 </script>
@@ -31,6 +67,15 @@ export default {
 @import "../node_modules/reveal.js/dist/reveal.css";
 @import "../node_modules/reveal.js/dist/theme/solarized.css";
 @import "../node_modules/reveal.js/plugin/highlight/zenburn.css";
+
+.reveal section p {
+    font-weight: bold !important;
+    font-size: 0.7em !important;
+}
+
+.reveal section pre code {
+    font-size: 0.7em !important;
+}
 
 /*#app {*/
 /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
